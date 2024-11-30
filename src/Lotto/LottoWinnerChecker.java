e3d09c805cccfca1453ba15996316372870735d8
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-11-30 17:32:31 +0900

    feat: 당첨 숫자 갯수를 반환하는 LottoWinnerChecker 클래스 작성

--- src/Lotto/LottoWinnerChecker.java
+++ src/Lotto/LottoWinnerChecker.java
@@ -1,4 +1,16 @@
 package Lotto;
 
 public class LottoWinnerChecker {
+
+    public int countMatching(int[] playerLottoTicket, int[] winningNumbers) {
+        int count = 0;
+        for (int target : playerLottoTicket) {
+            for (int winningNumber : winningNumbers) {
+                if (target == winningNumber) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
