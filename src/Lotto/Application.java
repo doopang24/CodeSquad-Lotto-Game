3c8c7656e6bac1f87d8fe9492a94b516b2834a83
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-11-30 15:41:09 +0900

    feat: main() 클래스 작성

--- src/Lotto/Application.java
+++ src/Lotto/Application.java
@@ -1,4 +1,12 @@
 package Lotto;
 
+import java.util.Arrays;
+
 public class Application {
+    public static void main(String[] args) {
+        PurchaseLottoTicket purchase = new PurchaseLottoTicket();
+
+        int[] playerLottoTicket = purchase.getLottoTicket();
+        System.out.println(Arrays.toString(playerLottoTicket));
+    }
 }
