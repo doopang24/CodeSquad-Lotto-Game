3b972ec16e0a26cccf13756b81de9bf58d648f00
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-11-30 17:32:53 +0900

    refactor: main() 메서드 수정

--- src/Lotto/Application.java
+++ src/Lotto/Application.java
@@ -3,9 +3,15 @@ package Lotto;
 public class Application {
     public static void main(String[] args) {
         LottoGenerator generator = new LottoGenerator();
-
-        int[] playerLottoTicket = generator.getLottoTicket();
+        LottoWinnerChecker checker = new LottoWinnerChecker();
+        ResultPrinter printer = new ResultPrinter();
 
         int[] winningNumbers = generator.getWinningNumbers();
+        int[] playerLottoTicket = generator.getLottoTicket();
+        int countMatch = checker.countMatching(playerLottoTicket, winningNumbers);
+
+        printer.printWinningNumbers(winningNumbers);
+        printer.printPlayerLotto(playerLottoTicket);
+        printer.printLotteryResult(countMatch);
     }
 }
