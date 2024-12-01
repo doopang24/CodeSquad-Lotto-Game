9788e38bf50a22b7bbcdafa835f7ac3e2448ff2b
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-01 13:15:06 +0900

    feat: 당첨 등급 enum에 상금 데이터 추가

--- src/Lotto/WinningLevel.java
+++ src/Lotto/WinningLevel.java
@@ -1,25 +1,27 @@
 package Lotto;
 
 public enum WinningLevel {
-    FIRST(6, "1등입니다."),
-    SECOND(5, true, "2등입니다."),
-    THIRD(5, "3등입니다."),
-    FOURTH(4, "4등입니다."),
-    FIFTH(3, "5등입니다."),
-    NONE(0, "낙첨입니다.");
+    FIRST(6, "1등입니다.", 1000000),
+    SECOND(5, true, "2등입니다.", 100000),
+    THIRD(5, "3등입니다.", 10000),
+    FOURTH(4, "4등입니다.", 5000),
+    FIFTH(3, "5등입니다.", 1000),
+    NONE(0, "낙첨입니다.", 0);
 
     private final int matchingNumber;
     private final boolean bonus;
     private final String description;
+    private final int prize;
 
-    WinningLevel(int matchingNumber, boolean bonus, String description) {
+    WinningLevel(int matchingNumber, boolean bonus, String description, int prize) {
         this.matchingNumber = matchingNumber;
         this.bonus = bonus;
         this.description = description;
+        this.prize = prize;
     }
 
-    WinningLevel(int matchingNumber, String description) {
-        this(matchingNumber, false, description);
+    WinningLevel(int matchingNumber, String description, int prize) {
+        this(matchingNumber, false, description, prize);
     }
 
     public static WinningLevel findWinningLevel(int matchingNumber, boolean hasBonus) {
@@ -33,5 +35,13 @@ public enum WinningLevel {
 
     public String getDescription() {
         return description;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getFormattedPrize() {
+        return String.format("%,d원", getPrize());
     }
 }
