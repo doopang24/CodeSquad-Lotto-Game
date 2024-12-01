c2cd28fc0bebe72733e486775543d17d06cfd701
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-01 16:22:58 +0900

    refactor: LottoWinnerChecker 클래스 수정

--- src/Lotto/LottoWinnerChecker.java
+++ src/Lotto/LottoWinnerChecker.java
@@ -16,12 +16,27 @@ public class LottoWinnerChecker {
 
     public boolean checkHasBonus(int[] playerLottoTicket, int bonus) {
         boolean check = false;
-        for(int lottoNumber : playerLottoTicket) {
-            if(lottoNumber == bonus) {
+        for (int lottoNumber : playerLottoTicket) {
+            if (lottoNumber == bonus) {
                 check = true;
                 break;
             }
         }
         return check;
+    }
+
+    public String findMatchingNumbers(int[] playerLottoTicket, int[] winningNumbers) {
+        StringBuilder builder = new StringBuilder();
+        for (int lottoNumber : playerLottoTicket) {
+            for (int winningNumber : winningNumbers) {
+                if (lottoNumber == winningNumber) {
+                    builder.append(lottoNumber).append(",");
+                }
+            }
+        }
+        if (!builder.isEmpty()) {
+            builder.deleteCharAt(builder.length() - 1);
+        }
+        return builder.toString();
     }
 }
